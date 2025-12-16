2ca21c7572685b93e956c40d1e7f3c3f47c92dd8
roqueotha04 <roqueotha04@gmail.com>
2025-12-16 18:09:24 -0300

    Added features to EventScheduleService and moved .gitignore to the root directory

--- ticket-manager/src/main/java/com/tickify/ticket_manager/services/EventScheduleService.java
+++ ticket-manager/src/main/java/com/tickify/ticket_manager/services/EventScheduleService.java
@@ -1,4 +1,15 @@
 package com.tickify.ticket_manager.services;
 
+import com.tickify.ticket_manager.entities.EventSchedule;
+
+import java.util.List;
+
 public interface EventScheduleService {
+    public EventSchedule addEventSchedule();
+
+    public List<EventSchedule> getEventScheduleByEventName (String eventName);
+
+    public void deleteEventSchedule (EventSchedule eventSchedule);
+
+    public EventSchedule updateEventSchedule (Long id, EventSchedule eventSchedule);
 }
